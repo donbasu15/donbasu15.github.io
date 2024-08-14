815d0c9db96f7de6ab0f77c8659b7149e79f28bb
Don Basumatary <donbasty375@gmail.com>
2024-08-14 15:44:56 +0530

    error file

--- src/Pages/NotFound.jsx
+++ src/Pages/NotFound.jsx
@@ -1,8 +1,16 @@
 function NotFound(){
     return(
-        <h3>
-            PAGE NOT FOUND
-        </h3>
+        <>
+           <Header></Header>
+           <div className="page-body">
+              <div className="body-parts"><br /><Sidebar></Sidebar></div>
+              <div className="body-parts">
+                <h2>Page not Found</h2>     
+             </div>
+           </div>
+           
+           
+        </>
     )
 }
 
